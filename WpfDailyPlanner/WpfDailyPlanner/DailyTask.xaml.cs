231a5b8432b8f51ec544089901853f0f77c0c76c
Микола Мантицький <slwpke447@gmail.com>
2019-04-26 12:17:09 +0300

    create+add some ui

---
+++ WpfDailyPlanner/WpfDailyPlanner/DailyTask.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfDailyPlanner
+{
+    /// <summary>
+    /// Interaction logic for DailyTask.xaml
+    /// </summary>
+    public partial class DailyTask : Window
+    {
+        public DailyTask()
+        {
+            InitializeComponent();
+            lb_today.Content += " " + DateTime.Now.ToString();
+        }
+    }
+}
